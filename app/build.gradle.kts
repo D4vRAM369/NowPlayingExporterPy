a2cc1b138b285076ad4cdedca66a3d2af65a6354
D4vRAM <d4vram369@proton.me>
2025-09-15 04:43:14 +0100

    release: bump to v1.2 (versionName/versionCode) + notas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.d4vram.nowplayingexporterpy"
         minSdk = 28
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 3
+        versionName = "1.2.0"
 
         ndk {
             abiFilters += listOf("arm64-v8a", "armeabi-v7a")
